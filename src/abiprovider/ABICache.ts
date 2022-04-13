fb293f52c58137cf31ff4e2bd7d977961b686607
sam bacha <sam@manifoldfinance.com>
2022-04-13 04:01:46 -0700

    feat(provider): cache

---
+++ src/abiprovider/ABICache.ts
@@ -0,0 +1,30 @@
+import keccak256 from 'keccak256'
+import { ABIElement, Address } from './types'
+
+export class ABICache {
+  // stores ABI elements by address by methodId
+  private readonly data: Record<string, Record<string, ABIElement>> = {}
+
+  has(address: string): boolean {
+    return !!this.data[address]
+  }
+
+  get(contract: Address): Record<string, ABIElement> | undefined {
+    return this.data[contract]
+  }
+
+  add(address: string, abi: ABIElement[]) {
+    const abiByMethodIds: Record<string, ABIElement> = {}
+    const onlyMethodsABI = abi.filter((a) => a.name && a.inputs)
+    for (const abiElement of onlyMethodsABI) {
+      const methodId = getMethodId(abiElement)
+      abiByMethodIds[methodId] = abiElement
+    }
+    this.data[address] = abiByMethodIds
+  }
+}
+
+function getMethodId(abiElement: ABIElement): string {
+  const signature = `${abiElement.name}(${abiElement.inputs.map((i) => i.type).join(',')})`
+  return '0x' + keccak256(signature).toString('hex').slice(0, 8)
+}
